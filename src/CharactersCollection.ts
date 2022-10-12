c6313b1dfed351adfffe5dca9105fb39b4f9b699
BJ Jackson <smthbrothrjdev@gmail.com>
2022-10-11 21:53:13 -0500

    cleaned up code

---
+++ src/CharactersCollection.ts
@@ -0,0 +1,24 @@
+import { Sortable } from "./Sorter";
+
+export class CharactersCollection implements Sortable {
+  constructor(public data: string) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return (
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
+    );
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const characters = [...this.data];
+
+    const leftHand = characters[leftIndex];
+    characters[leftIndex] = characters[rightIndex];
+    characters[rightIndex] = leftHand;
+    this.data = characters.join("");
+  }
+}
