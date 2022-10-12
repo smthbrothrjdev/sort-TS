ac656b442af85b1ded1fa16094cc84552ad9e23a
BJ Jackson <smthbrothrjdev@gmail.com>
2022-10-11 21:52:59 -0500

    cleaned up code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import { NumbersCollection } from "./NumbersCollection";
-import { StringsCollection } from "./StringsCollection";
+import { CharactersCollection } from "./CharactersCollection";
 import { Sorter } from "./Sorter";
-
-const numberArray = new NumbersCollection([1, -3, 25, 7]);
-const numSorter = new Sorter(numberArray);
-numSorter.sort();
-console.log(`sorted array: ${numberArray.data}`);
-
-const stringThing = new StringsCollection("hello");
-const charSorter = new Sorter(stringThing);
-charSorter.sort();
-console.log(stringThing.data);
